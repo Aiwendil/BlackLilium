d3b1c1752457c07f8b6e082150a7ac00f22791f2
Aiwendil <AiwendilH@gmail.com>
2013-07-23 14:11:45 +0200

    Added license and very basic project structure

---
+++ src/engine/engine.h
@@ -0,0 +1,33 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright (C) 2013  Fabian Stock <AiwendilH@gmail.com>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ENGINE_H
+#define ENGINE_H
+
+class Engine
+{
+  public:
+    Engine();
+    Engine(const Engine &other);
+    ~Engine();
+    Engine &operator=(const Engine &other);
+    bool operator==(const Engine &other);
+
+};
+
+#endif // ENGINE_H
